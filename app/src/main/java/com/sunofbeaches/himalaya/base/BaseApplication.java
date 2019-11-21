1bd1ee62a91be53ffe656fdc671bfba9ecd2d351
849077547 <849077547@qq.com>
2019-11-21 22:07:44 +0800

    update last codes.

--- app/src/main/java/com/sunofbeaches/himalaya/base/BaseApplication.java
+++ app/src/main/java/com/sunofbeaches/himalaya/base/BaseApplication.java
@@ -24,23 +24,23 @@ public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         CommonRequest mXimalaya = CommonRequest.getInstanse();
-        if (DTransferConstants.isRelease) {
+        if(DTransferConstants.isRelease) {
             String mAppSecret = "8646d66d6abe2efd14f2891f9fd1c8af";
             mXimalaya.setAppkey("9f9ef8f10bebeaa83e71e62f935bede8");
             mXimalaya.setPackid("com.app.test.android");
-            mXimalaya.init(this, mAppSecret);
+            mXimalaya.init(this,mAppSecret);
         } else {
             String mAppSecret = "0a09d7093bff3d4947a5c4da0125972e";
             mXimalaya.setAppkey("f4d8f65918d9878e1702d49a8cdf0183");
             mXimalaya.setPackid("com.ximalaya.qunfeng");
-            mXimalaya.init(this, mAppSecret);
+            mXimalaya.init(this,mAppSecret);
         }
 
         //初始始化播放器
         XmPlayerManager.getInstance(this).init();
 
         //初始化LogUtil
-        LogUtil.init(this.getPackageName(), false);
+        LogUtil.init(this.getPackageName(),false);
 
         sHandler = new Handler();
 
